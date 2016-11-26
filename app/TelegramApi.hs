8ea5c5451d609329ec354c8faf462cf9726fad91
Kemphack <kemphack@mail.ru>
2016-11-26 16:52:27 +0300

    bot v0.1-rep 4

--- app/TelegramApi.hs
+++ app/TelegramApi.hs
@@ -1,5 +1,7 @@
 module TelegramApi
-    ( sendMessage
+    ( 
+        sendMessage,
+        getHttpRequest,
     ) where
 
 import Network.HTTP.Client       (httpLbs, responseStatus, responseBody, newManager, parseRequest)
@@ -7,10 +9,13 @@ import Network.HTTP.Client.TLS   (tlsManagerSettings)
 import Network.HTTP.Types.Status (statusCode)
 import Data.ByteString.Lazy      (ByteString)
 
-sendMessage :: String -> String -> IO ByteString
-sendMessage token message = do 
+getHttpRequest :: String -> IO ByteString
+getHttpRequest url = do 
     manager <- newManager tlsManagerSettings
-    request <- parseRequest ("https://api.telegram.org/bot" ++ token ++ "/sendMessage?text=" ++ message ++ "&chat_id=193856114")
+    request <- parseRequest url
     response <- httpLbs request manager
     putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
-    pure . responseBody $ response
+    pure $ responseBody response
+
+sendMessage :: String -> String -> IO ByteString
+sendMessage token message = getHttpRequest ("https://api.telegram.org/bot" ++ token ++ "/sendMessage?text=" ++ message ++ "&chat_id=193856114")
