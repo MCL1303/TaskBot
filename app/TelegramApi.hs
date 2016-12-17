5fdc1c73127e61a224e7a566de483a8e27039d5d
Kemphack <kemphack@mail.ru>
2016-12-17 22:56:18 +0300

    UTF-8 Encoding

--- app/TelegramApi.hs
+++ app/TelegramApi.hs
@@ -1,26 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
-module TelegramApi
-    ( 
-        sendMessage,
-        getHttpRequest,
-    ) where
 
-import Network.HTTP.Client                    (httpLbs, responseStatus, responseBody, newManager, parseRequest)
-import Network.HTTP.Client.TLS                (tlsManagerSettings)
-import Network.HTTP.Types.Status              (statusCode)
-import qualified Data.ByteString.Lazy as Lazy (ByteString)
-import Data.Text.Encoding                     (encodeUtf8)
-import Network.HTTP.Conduit                   (setQueryString)
+module TelegramApi
+(
+    sendMessage,
+    getHttpRequest,
+) where
+
 import Data.ByteString                        (ByteString)
 import Data.Text                              (pack)
+import Data.Text.Encoding                     (encodeUtf8)
+import Network.HTTP.Client                    (httpLbs, responseStatus, responseBody, newManager, parseRequest)
+import Network.HTTP.Client.TLS                (tlsManagerSettings)
+import Network.HTTP.Conduit                   (setQueryString)
+import Network.HTTP.Types.Status              (statusCode)
+import qualified Data.ByteString.Lazy as Lazy (ByteString)
 
 getHttpRequest :: String -> [(ByteString, Maybe ByteString)] -> IO Lazy.ByteString
-getHttpRequest url args = do 
+getHttpRequest url args = do
     manager <- newManager tlsManagerSettings
-    request <- parseRequest url 
+    request <- parseRequest url
     response <- httpLbs (setQueryString args request) manager
     putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
     pure $ responseBody response
 
 sendMessage :: String -> String -> IO Lazy.ByteString
-sendMessage token message = getHttpRequest ("https://api.telegram.org/bot" ++ token ++ "/sendMessage") [("text", Just $ encodeUtf8 (pack message)),("chat_id",Just $ encodeUtf8 "193856114")]
+sendMessage token message = getHttpRequest url params
+    where params = [("text", Just $ encodeUtf8 (pack message))
+                    , ("chat_id",Just $ encodeUtf8 "193856114")]
+          url = ("https://api.telegram.org/bot" ++ token ++ "/sendMessage")
