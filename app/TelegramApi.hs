99136e6f438d1333d83c047287f133e72ab3bde6
kemphack <kemphack@mail.ru>
2016-12-18 00:35:31 +0300

    UTF8-encoding (#6)
    * Исправлена проблема с отправлением сообщений. Теперь их можно отправлять на русском
    
    * UTF-8 Encoding
    
    * Travis conf
    
    * Travis pref
    
    * Travis pref
    
    * UTF8-encoding

--- app/TelegramApi.hs
+++ app/TelegramApi.hs
@@ -1,21 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module TelegramApi
-    ( 
-        sendMessage,
-        getHttpRequest,
-    ) where
+(
+    sendMessage,
+    getHttpRequest,
+) where
 
-import Network.HTTP.Client       (httpLbs, responseStatus, responseBody, newManager, parseRequest)
-import Network.HTTP.Client.TLS   (tlsManagerSettings)
-import Network.HTTP.Types.Status (statusCode)
-import Data.ByteString.Lazy      (ByteString)
+import Data.ByteString                        (ByteString)
+import Data.Text                              (pack)
+import Data.Text.Encoding                     (encodeUtf8)
+import Network.HTTP.Client                    (httpLbs
+                                              , responseStatus
+                                              , responseBody
+                                              , newManager
+                                              , parseRequest
+                                              )
+import Network.HTTP.Client.TLS                (tlsManagerSettings)
+import Network.HTTP.Conduit                   (setQueryString)
+import Network.HTTP.Types.Status              (statusCode)
+import qualified Data.ByteString.Lazy as Lazy (ByteString)
 
-getHttpRequest :: String -> IO ByteString
-getHttpRequest url = do 
+getHttpRequest :: String -> [(ByteString, Maybe ByteString)] -> IO Lazy.ByteString
+getHttpRequest url args = do
     manager <- newManager tlsManagerSettings
     request <- parseRequest url
-    response <- httpLbs request manager
+    response <- httpLbs (setQueryString args request) manager
     putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
     pure $ responseBody response
 
-sendMessage :: String -> String -> IO ByteString
-sendMessage token message = getHttpRequest ("https://api.telegram.org/bot" ++ token ++ "/sendMessage?text=" ++ message ++ "&chat_id=193856114")
+sendMessage :: String -> String -> IO Lazy.ByteString
+sendMessage token message = getHttpRequest url params
+    where params = [("text", Just $ encodeUtf8 (pack message))
+                   , ("chat_id",Just $ encodeUtf8 "193856114")
+                   ]
+          url = ("https://api.telegram.org/bot" ++ token ++ "/sendMessage")
