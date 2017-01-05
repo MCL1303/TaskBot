3a7c7a1425b96ee5ab3d7c97a93a586d6615c890
Kemphack <kemphack@mail.ru>
2017-01-05 22:01:19 +0300

    TokenFile

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,29 @@
-module Main where
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import TelegramApi(sendMessage)
+module Main (main) where
+
+import           Control.Exception (IOException, catch)
+import           System.Exit       (exitFailure)
+import           System.IO         (hPutStr, readFile, stderr)
+import           System.IO.Error   (isDoesNotExistError)
+
+import           TelegramApi       (sendMessage)
+
+putLog a = hPutStr stderr (a ++ "\n")
+
+loadToken :: String -> IO String
+loadToken fileName = do
+    catch (readFile fileName) handler
+  where
+      handler e
+          |isDoesNotExistError e = do
+              putLog "Couldn't find token file."
+              exitFailure
+          |otherwise = do
+              putLog (show e)
+              exitFailure
 
 main :: IO ()
 main = do
-    token <- getLine
-    messageText <- getLine
-    mes <- sendMessage token messageText
-    print $ mes
+    str <- loadToken ""
+    print $ str
