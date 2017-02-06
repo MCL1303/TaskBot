2cef4331da1b7b75f83aaaa866613666213fba43
Kemphack <kemphack@mail.ru>
2017-02-06 22:22:59 +0300

    Token Loader

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,51 @@ module Main (main) where
 
 import           Control.Exception (IOException, catch)
 import           System.Exit       (exitFailure)
-import           System.IO         (hPutStr, readFile, stderr)
+import           System.IO         (hPutStrLn, readFile, stderr)
 import           System.IO.Error   (isDoesNotExistError)
 
-import           TelegramApi       (sendMessage)
+import           TelegramApi (Chat (chtId), Message (msgChat, msgText),
+                              Update (updMessage, updUpdate_id),
+                              getLastMessages, sendMessage)
 
-putLog a = hPutStr stderr (a ++ "\n")
+putLog a = hPutStrLn stderr a
+
+processUpdates
+    :: String -- ^ Token
+    -> [Update] -- ^ List of updates
+    -> Maybe Int -- ^ Offset(current update id)
+    -> IO (Maybe Int) -- ^ New offset
+processUpdates token updates offset = do
+    case updates of
+        []   -> case offset of
+            Nothing -> pure offset
+            Just a  -> pure (Just a)
+        x:xs -> do
+            msg <- case updMessage x of
+                Just message ->
+                    sendMessage
+                        token
+                        (getMessageText message)
+                        (chtId (msgChat message))
+            writeParam updateIdFile (updUpdate_id x)
+            processUpdates token xs (Just (updUpdate_id x + 1))
+  where
+    getMessageText a = case msgText a of
+        Nothing      -> ""
+        Just message -> message
+
+bot
+    :: String -- ^ Token
+    -> Maybe Int -- ^ Offset(Update id)
+    -> IO ()
+bot token curOffset = do
+    mUpdates <- getLastMessages token curOffset
+    case mUpdates of
+        Nothing -> do
+            bot token curOffset
+        Just updates  -> do
+            newOffset <- processUpdates token updates curOffset
+            bot token newOffset
 
 loadToken :: String -> IO String
 loadToken fileName = do
@@ -25,7 +64,7 @@ loadToken fileName = do
 
 main :: IO ()
 main = do
-    str <- loadToken filePath
-    print str
+    token <- loadToken filePath
+    bot Noting token
   where
-  	filePath = "token.txt"
+    filePath = "token.txt"
