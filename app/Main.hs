3706742268779a95d92c08905d6597b681eee447
Kemphack <kemphack@mail.ru>
2017-03-04 19:40:54 +0300

    using telegram-api

--- app/Main.hs
+++ app/Main.hs
@@ -3,45 +3,37 @@
 module Main (main) where
 
 import           ClassyPrelude           (tshow)
+import           Control.Concurrent      (threadDelay)
 import           Data.Foldable           (for_)
-import           Data.Maybe              (fromMaybe)
-import           Data.Text               (empty, pack, strip)
-import           Network.HTTP.Client     (newManager, Manager)
+import           Network.HTTP.Client     (Manager, newManager)
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
 import           Safe                    (lastMay)
-import           System.IO               (hPutStrLn, stderr)
-import           Tools                   (readParam, readParamString,
-                                          writeParam)
+import           Tools                   (putLog, readOffset, readToken,
+                                          saveOffset)
 import           Web.Telegram.API.Bot    (Chat (..), Message (..),
-                                          Response (..), Token (Token),
-                                          Update (..),
-                                          getUpdates, sendMessage,
+                                          Response (..), Token (..),
+                                          Update (..), getUpdates, sendMessage,
                                           sendMessageRequest)
 
 -- | Path to file which contains current update id
 updateIdFile :: String
 updateIdFile = "update_id.txt"
 
--- | Puts message in log
-putLog :: String -> IO()
-putLog = hPutStrLn stderr
-
-handleMessage :: Token -> Update -> Manager -> IO ()
-handleMessage token Update{update_id = updateId, message = messageM} manager = do
-    case (messageM) of
-        Just Message{chat = Chat{chat_id = chatId}, text = mText} -> do
-            case mText of
-                Just text -> do
+handleMessage :: Token -> Manager -> Update -> IO ()
+handleMessage token manager Update{update_id, message} = do
+    case message of
+        Just Message{chat = Chat{chat_id}, text} -> do
+            case text of
+                Just jText -> do
                     res <- sendMessage
                         token
-                        (sendMessageRequest (tshow chatId) text)
+                        (sendMessageRequest (tshow chat_id) jText)
                         manager
                     case res of
                         Left e  ->
                             putLog ("Message request failed. " ++ (show e))
                         Right _ -> do
-                            writeParam updateIdFile updateId
-                            pure ()
+                            saveOffset updateIdFile update_id
                 Nothing -> pure()
         Nothing   -> pure()
 
@@ -53,26 +45,22 @@ bot
 bot token curOffset manager = do
     mUpdates <- getUpdates token curOffset Nothing Nothing manager
     case mUpdates of
-        Right Response{result = updates} -> do
-            for_ updates (\a -> handleMessage token a manager)
-            case (lastMay updates) of
-                Just lastM -> do
-                    let newOffset = update_id lastM + 1
-                    bot token (Just (newOffset)) manager
+        Right Response{result} -> do
+            for_ result (handleMessage token manager)
+            case (lastMay result) of
+                Just Update{update_id} -> do
+                    let newOffset = update_id + 1
+                    bot token (Just newOffset) manager
                 Nothing    -> bot token curOffset manager
         Left uError    -> do
             putLog (show uError)
+            threadDelay 5000
             bot token curOffset manager
 
 main :: IO ()
 main = do
-    pOffset <- readParam updateIdFile
-    tokenFromFile <- readParamString tokenFile
-    manager <- newManager tlsManagerSettings
-    let offset = case pOffset of
-            Right mOffset -> Just mOffset
-            Left  _       -> Nothing
-    case tokenFromFile of
-        Right rawToken -> bot (Token (strip (pack ("bot" ++ rawToken)))) offset manager
-        Left  e     -> putLog ("Error opening " ++ tokenFile ++ "file. " ++ (show e))
+    offset   <- readOffset updateIdFile
+    token    <- readToken tokenFile
+    manager  <- newManager tlsManagerSettings
+    bot token offset manager
   where tokenFile = "token.txt"
--- app/Tools.hs
+++ app/Tools.hs
@@ -3,16 +3,26 @@ module Tools
     -- * deriving tools
     drvJS,
     -- * I/O tools
-    readParam,
-    readParamString,
-    writeParam
+    readOffset,
+    readToken,
+    saveOffset,
+    -- * Log tool
+    putLog
 ) where
 
 import           Control.Exception          (IOException, try)
 import           Data.Aeson.TH              (Options (constructorTagModifier, fieldLabelModifier),
                                              defaultOptions, deriveJSON)
 import           Data.Char                  (toLower)
+import           Data.Text                  (pack, strip)
 import           Language.Haskell.TH.Syntax (Dec, Name, Q)
+import           System.Exit                (exitFailure)
+import           System.IO                  (hPutStrLn, stderr)
+import           Web.Telegram.API.Bot       (Token (Token))
+
+-- | Puts message in log
+putLog :: String -> IO()
+putLog errorMessage = hPutStrLn stderr errorMessage
 
 drvJS :: Name -> Q [Dec]
 drvJS bm = deriveJSON options bm
@@ -22,18 +32,30 @@ drvJS bm = deriveJSON options bm
         , constructorTagModifier = map toLower
         }
 
-readParam :: (Read a) => String -> IO (Either IOException a)
-readParam fileName = do
-    param <- readParamString fileName
-    let eParam = case param of
-            Right string -> Right (read string)
-            Left e       -> Left e
-    pure eParam
+readToken :: String -> IO Token
+readToken fileName = do
+    eToken <- try (readFile fileName) :: IO(Either IOException String)
+    case eToken of
+        Right token -> pure (Token(strip (pack ("bot" ++ token))))
+        Left e      -> do
+            putLog ("Error reading offset from " ++ fileName)
+            putLog (show e)
+            exitFailure
 
-readParamString :: String -> IO (Either IOException String)
-readParamString fileName = do
-    eResult <- try (readFile fileName) :: IO (Either IOException String)
-    pure eResult
+readOffset :: String -> IO (Maybe Int)
+readOffset fileName = do
+    eOffset <- try (readFile fileName) :: IO(Either IOException String)
+    case eOffset of
+        Right offsetString -> pure(read offsetString)
+        Left  e            -> do
+            putLog(show e)
+            pure Nothing
 
-writeParam :: (Show a) => String -> a -> IO ()
-writeParam fileName param = writeFile fileName (show param)
+saveOffset :: String -> Int -> IO ()
+saveOffset fileName offset = writeFile fileName (show offset)
+   {- case result of
+        Right _ -> pure ()
+        Left e  -> do
+            putLog("Error saving offset into " ++ fileName)
+            putLog(show e)
+            exitFailure-}
